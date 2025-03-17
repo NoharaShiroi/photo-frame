fac9c315566aed529f25abdb04404a2b0bc6ceeb
NoharaShiroi <sheng8213@gmail.com>
2025-03-17 14:50:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
+<!-- script.js -->
 const app = {
-    CLIENT_ID: "1004388657829-mvpott95dsl5bapu40vi2n5li7i7t7d1.apps.googleusercontent.com", 
-    REDIRECT_URI: "https://noharashiroi.github.io/photo-frame/", 
+    CLIENT_ID: "YOUR_CLIENT_ID", 
+    REDIRECT_URI: "YOUR_REDIRECT_URI", 
     SCOPES: "https://www.googleapis.com/auth/photoslibrary.readonly",
     accessToken: sessionStorage.getItem("access_token") || null,
     albumId: null,
     photos: [],
     currentPhotoIndex: 0,
     nextPageToken: null,
-    slideshowInterval: null, 
-    isPlaying: false, 
-    isLoading: false, 
-    slideshowSpeed: 3000, 
-    currentAlbumId: null, 
+    slideshowInterval: null,
+    isLoading: false,
+    slideshowSpeed: 5000,
 
     getAccessToken: function() {
         let hashParams = new URLSearchParams(window.location.hash.substring(1));
@@ -24,7 +23,7 @@ const app = {
             document.getElementById("auth-container").style.display = "none";
             document.getElementById("app-container").style.display = "flex";
             this.fetchAlbums();
-            this.fetchPhotos.bind(this)(); 
+            this.fetchPhotos();
         } else {
             document.getElementById("auth-container").style.display = "flex";
             document.getElementById("app-container").style.display = "none";
@@ -37,79 +36,34 @@ const app = {
     },
 
     fetchAlbums: function() {
-    if (!this.accessToken) return;
-    fetch("https://photoslibrary.googleapis.com/v1/albums?pageSize=50", {
-        method: "GET",
-        headers: { "Authorization": "Bearer " + this.accessToken }
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.albums) {
-            this.renderAlbumList(data.albums);
-        }
-    })
-    .catch(error => console.error("Error fetching albums:", error));
-}.bind(app),  // 在事件綁定時確保 this 指向 app
-    
-    fetchPhotos: function() {
-        if (this.isLoading) return;
-        this.isLoading = true;
-
-        fetch("https://photoslibrary.googleapis.com/v1/mediaItems:search", {
-            method: "POST",
-            headers: { "Authorization": "Bearer " + this.accessToken, "Content-Type": "application/json" },
-            body: JSON.stringify({ pageSize: 50, pageToken: this.nextPageToken || '', albumId: this.albumId })
+        if (!this.accessToken) return;
+        fetch("https://photoslibrary.googleapis.com/v1/albums?pageSize=50", {
+            method: "GET",
+            headers: { "Authorization": "Bearer " + this.accessToken }
         })
         .then(response => response.json())
         .then(data => {
-            if (data.mediaItems) {
-                this.photos.push(...data.mediaItems);
-                this.nextPageToken = data.nextPageToken;
-                this.renderPhotos();
+            if (data.albums) {
+                this.renderAlbumList(data.albums);
             }
         })
-        .catch(error => console.error("Error fetching photos:", error))
-        .finally(() => this.isLoading = false);
+        .catch(error => console.error("Error fetching albums:", error));
     },
 
-    renderPhotos: function() {
-        let container = document.getElementById("photo-container");
-        container.innerHTML = "";
-        
-        if (this.photos.length === 0) {
-            container.innerHTML = "<p>此相簿沒有照片</p>";
-        } else {
-            this.photos.forEach((photo, index) => {
-                let img = document.createElement("img");
-                img.src = `${photo.baseUrl}=w600-h400`;
-                img.alt = "Photo";
-                img.classList.add("photo");
-                img.onclick = () => this.openLightbox(index);
-                container.appendChild(img);
-            });
-        }
-    },
-
-    startSlideshow: function() {
-        if (this.photos.length > 0) {
-            let speedInput = document.getElementById("slideshow-speed");
-            this.slideshowSpeed = Math.max(1000, speedInput.value * 1000); 
-            this.autoChangePhoto(); 
-        }
-    },
-
-    autoChangePhoto: function() {
-        clearInterval(this.slideshowInterval);
-        this.slideshowInterval = setInterval(() => {
-            this.currentPhotoIndex = (this.currentPhotoIndex + 1) % this.photos.length;
-            this.showCurrentPhoto();
-        }, this.slideshowSpeed);
-    },
+    renderAlbumList: function(albums) {
+        let albumSelect = document.getElementById("album-select");
+        albumSelect.innerHTML = '<option value="all">所有相片</option>';
+        albums.forEach(album => {
+            let option = document.createElement("option");
+            option.value = album.id;
+            option.textContent = album.title;
+            albumSelect.appendChild(option);
+        });
+    }
 };
 
-// 事件監聽
 window.onload = function() {
-    document.getElementById("authorize-btn").onclick = app.authorizeUser.bind(app);
-    document.getElementById("start-slideshow-btn").onclick = app.startSlideshow.bind(app);
+    document.getElementById("authorize-btn").onclick = () => app.authorizeUser();
+    document.getElementById("start-slideshow-btn").onclick = () => app.startSlideshow();
     app.getAccessToken();
 };
