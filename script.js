8540cbecb915627bc0e81f80c773caff51d1b664
NoharaShiroi <sheng8213@gmail.com>
2025-06-12 10:58:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const app = {
     CLIENT_ID: "1004388657829-mvpott95dsl5bapu40vi2n5li7i7t7d1.apps.googleusercontent.com",
     REDIRECT_URI: "https://noharashiroi.github.io/photo-frame/",
-    SCOPES:  "https://www.googleapis.com/auth/photoslibrary.readonly",
-             "https://www.googleapis.com/auth/photoslibrary.sharing",
+
+const SCOPES = [
+                 'https://www.googleapis.com/auth/photoslibrary.readonly',
+                 'https://www.googleapis.com/auth/photoslibrary.sharing'
+               ].join(' ');
     
     states: {
         accessToken: null,
